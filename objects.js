f0d7cf528575c7ac310c2f4eb2cb08c315c62505
warren-west <teacher.warren.english@gmail.com>
2026-02-17 10:29:33 +0100

    added objects example

---
+++ objects.js
@@ -0,0 +1,25 @@
+const simpleObject = { singleKey: "value" }
+
+const myObject = {
+    firstName: "Warren",
+    lastName: "West",
+    age: 32,
+    isMarried: true, // a trailing comma
+    gender: "Male",
+}
+
+let animal = {
+    weight: 50,
+    isCarnivore: true,
+    emoji: "🦁",
+    location: "Africa"
+}
+
+let product = {
+    costPrice: 19.99,
+    markupPerc: 120,
+    description: "A can of soda",
+    brand: "Generic Cola",
+    stock: 20,
+    hasSugarTax: true
+}
